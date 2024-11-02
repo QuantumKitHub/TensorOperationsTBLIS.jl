d2f747c929e4aacce710b04471b9adc6a0bc4a0b
Jutho <jutho.haegeman@ugent.be>
2024-11-02 23:04:42 +0100

    add TBLIS docstring

--- src/TensorOperationsTBLIS.jl
+++ src/TensorOperationsTBLIS.jl
@@ -23,7 +23,14 @@ set_num_tblis_threads(n) = LibTBLIS.tblis_set_num_threads(convert(Cuint, n))
 
 # TensorOperations
 #------------------
+"""
+    TBLIS()
 
+Backend for tensor operations on strided arrays (arrays that can be cast into a `StridedView`)
+that uses [TBLIS](https://github.com/devinamatthews/tblis) library. This library can perform
+tensor contractions without additional intermediate tensors. It does not use LinearAlgebra
+or BLAS.
+"""
 struct TBLIS <: TensorOperations.AbstractBackend end
 
 Base.@deprecate(tblisBackend(), TBLIS())
